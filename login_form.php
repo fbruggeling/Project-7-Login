2eb2f308d5bd0047994d4c1c55b96c2ae4b063d7
fbruggeling <40127266+fbruggeling@users.noreply.github.com>
2023-02-07 12:08:32 +0100

    login_form gemaakt

---
+++ login_form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <title>Login pagina</title>
+</head>
+<body>
+    <h3>Login systeem Project 7</h3>
+
+    <form action="" method="POST">
+        <h4>Inloggen</h4>
+        <hr>
+
+        <label>Username:</label>
+        <input type="text" name="username" />
+        <br>
+        <label>Wachtwoord:</label>
+        <input type="text" name="password" />
+        <br>
+        <button name="login-btn">Login</button>
+        <br>
+        <a href="">Registratie</a>
+    </form>
+
+</body>
+</html>
